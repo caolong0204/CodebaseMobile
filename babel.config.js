e0feda10bdaf2acbae3cab34c969baba1ba61ed3
Long Vũ <vucaolong0204@gmail.com>
2023-02-09 14:58:24 +0700

    feat:add babel-module-resolver
    feat:update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,41 @@
+const alias = {
+  '@assets': './assets',
+  '@common': './common',
+  '@utils': './common/utils',
+  '@navigators': './navigators',
+  '@network': './network',
+  '@components': './components',
+  '@screens': './screens',
+  '@services': './services',
+  '@store': './store',
+  '@themes': './themes',
+  '@types': './types',
+};
+const plugins = [
+  [
+    'module-resolver',
+    {
+      root: ['./src/'],
+      extensions: [
+        '.ios.js',
+        '.android.js',
+        '.js',
+        '.jsx',
+        '.ts',
+        '.tsx',
+        '.json',
+      ],
+      alias,
+    },
+  ],
+];
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins,
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
 };
